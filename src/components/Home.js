e9657086d41174a3cca70e011c703777e5e346dc
SudhanshuHRI <salil221254@gmail.com>
2024-05-02 20:33:11 +0530

    homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,58 @@
+import * as React from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+
 const Home = () => {
-    return ( 
+
+    const [value, setValue] = React.useState(2);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+    console.log(value)
+    return (
         <div>
-            <h1>This is home page.</h1>
+            <div>
+                <div>
+                    <nav className='d-flex justify-content-between'>
+                        {/* logo and search box */}
+                        <div>
+                            {/* <img src="#"/> */}
+                            <div>
+                                <span>icon</span>
+                                <input type="text" />
+                            </div>
+
+                        </div>
+
+                        {/* tabs section */}
+                        <div>
+                            <Tabs value={value} onChange={handleChange} aria-label="disabled tabs example">
+                                <Tab label="Active" />
+                                <Tab label="Disabled" />
+                                <Tab label="Active" />
+                                <Tab label="Active" />
+                                <Tab label="Active" />
+                            </Tabs>
+
+                        </div>
+
+                        {/* navbar icons */}
+
+                        <div>
+                            <span>icon1</span>
+                            <span>icon2</span>
+                            <span>icon3</span>
+                            <span>icon4</span>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+            <div>
+                <h1>This is home page.</h1>
+            </div>
         </div>
-     );
+    );
 }
- 
+
 export default Home;
