9cb29ba9f84215bccba08708c418d67b3010d84f
SudhanshuHRI <salil221254@gmail.com>
2024-05-06 17:49:02 +0530

    nav done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,16 @@
 import * as React from 'react';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
+import { FaFacebook } from "react-icons/fa";
+import { IoSearch } from "react-icons/io5";
+import { GoHomeFill } from "react-icons/go";
+import { MdOutlineOndemandVideo } from "react-icons/md";
+import { AiOutlineShop } from "react-icons/ai";
+import { RiGroup2Line } from "react-icons/ri";
+import { FaGamepad } from "react-icons/fa6";
+import { TbGridDots } from "react-icons/tb";
+import { FaFacebookMessenger } from "react-icons/fa";
+import { RiNotification2Fill } from "react-icons/ri";
 
 const Home = () => {
 
@@ -14,13 +24,16 @@ const Home = () => {
         <div>
             <div>
                 <div>
-                    <nav className='d-flex justify-content-between'>
+                    <nav className='d-flex justify-content-between p-1'>
                         {/* logo and search box */}
-                        <div>
-                            {/* <img src="#"/> */}
+                        <div className='d-flex align-items-center logoDiv'>
                             <div>
-                                <span>icon</span>
-                                <input type="text" />
+                                <span className='fb-logo'><FaFacebook /></span>
+                            </div>
+
+                            <div className='searchBox'>
+                                <span><IoSearch /></span>
+                                <input type="text" placeholder='Search Facebook'/>
                             </div>
 
                         </div>
@@ -28,28 +41,45 @@ const Home = () => {
                         {/* tabs section */}
                         <div>
                             <Tabs value={value} onChange={handleChange} aria-label="disabled tabs example">
-                                <Tab label="Active" />
-                                <Tab label="Disabled" />
-                                <Tab label="Active" />
-                                <Tab label="Active" />
-                                <Tab label="Active" />
+                                <Tab label={<GoHomeFill />} className='tabIcons'/>
+                                <Tab label={<MdOutlineOndemandVideo />} className='tabIcons'/>
+                                <Tab label={<AiOutlineShop />} className='tabIcons'/>
+                                <Tab label={<RiGroup2Line />} className='tabIcons'/>
+                                <Tab label={<FaGamepad />} className='tabIcons'/>
                             </Tabs>
 
                         </div>
 
                         {/* navbar icons */}
 
-                        <div>
-                            <span>icon1</span>
-                            <span>icon2</span>
-                            <span>icon3</span>
-                            <span>icon4</span>
+                        <div className='navRightDiv'>
+                            <span className='navRight '><TbGridDots /></span>
+                            <span className='navRight'><FaFacebookMessenger /></span>
+                            <span className='navRight'><RiNotification2Fill /></span>
+                            <span className=' useimg'><img src="/resources/unnamed.jpg"  alt="userimg"/></span>
                         </div>
                     </nav>
                 </div>
             </div>
-            <div>
-                <h1>This is home page.</h1>
+            <div className='contailer-fluid'>
+                <div className='row'>
+                    <div className='col-3'>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                    </div>
+                    <div className='col-6'>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                    </div>
+                    <div className='col-3'>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                    </div>
+                </div>
+
             </div>
         </div>
     );
